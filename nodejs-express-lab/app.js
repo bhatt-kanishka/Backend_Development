d14784f5c54fc111fafc3ab4a23e81def68d1f79
bhatt-kanishka <kanishkabhatt93@gmail.com>
2026-08-20 18:19:24 +0530

    Add FastAPI Task 3

--- nodejs-express-lab/app.js
+++ nodejs-express-lab/app.js
@@ -94,7 +94,7 @@ app.get('/home', (req, res) => {
 
 app.get('/users', (req, res) => {
   const users = [
-    { id: 1, name: 'John Doe', email: 'john@example.com' },
+    { id: 1, name: 'kANISHKA BHATT', email: 'john@example.com' },
     { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
     { id: 3, name: 'Bob Johnson', email: 'bob@example.com' }
   ];
